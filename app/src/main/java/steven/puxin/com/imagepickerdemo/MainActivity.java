d2660752bf284678feb3a504963040cb8d552c9c
gaotaiwen <gaotaiwen@whyhowinfo.com>
2019-12-19 20:13:36 +0800

    合入androidx 装逼神器，修复图片的选中复选框背景混乱的bug

--- app/src/main/java/steven/puxin/com/imagepickerdemo/MainActivity.java
+++ app/src/main/java/steven/puxin/com/imagepickerdemo/MainActivity.java
@@ -1,16 +1,13 @@
 package steven.puxin.com.imagepickerdemo;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.lzy.imagepicker.ImagePicker;
 import com.lzy.imagepicker.ui.ImageGridActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showDialog();
-                ImagePicker.getInstance().setSelectLimit(1);
                 Intent intent1 = new Intent(MainActivity.this, ImageGridActivity.class);
                 startActivityForResult(intent1, REQUEST_CODE_SELECT);
             }
@@ -39,8 +35,8 @@ public class MainActivity extends AppCompatActivity {
         imagePicker.setShowCamera(true);                      //显示拍照按钮
         imagePicker.setCrop(false);                           //允许裁剪（单选才有效）
         imagePicker.setSaveRectangle(false);                   //是否按矩形区域保存
-        imagePicker.setMultiMode(false);
-        imagePicker.setSelectLimit(1);              //选中数量限制
+        imagePicker.setMultiMode(true);
+        imagePicker.setSelectLimit(9);              //选中数量限制
 //        imagePicker.setStyle(CropImageView.Style.CIRCLE);  //裁剪框的形状
 //        imagePicker.setFocusWidth(DensityUtil.dp2px(300));                       //裁剪框的宽度。单位像素（圆形自动取宽高最小值）
 //        imagePicker.setFocusHeight(DensityUtil.dp2px(300));                      //裁剪框的高度。单位像素（圆形自动取宽高最小值）
